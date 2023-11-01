2515d061f14d0814a0d4ee075cbd9b6c6f24f4be
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-11-01 19:11:40 +0600

    use imgbb for images hosting

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import "./App.css";
 import ImageGallery from "./components/ImageGallery";
-import img1 from "./images/image-1.webp";
-import img2 from "./images/image-2.webp";
-import img3 from "./images/image-3.webp";
-import img4 from "./images/image-4.webp";
-import img5 from "./images/image-5.webp";
-import img6 from "./images/image-6.webp";
-import img7 from "./images/image-7.webp";
-import img8 from "./images/image-8.webp";
-import img9 from "./images/image-9.webp";
-import img10 from "./images/image-10.jpeg";
-import img11 from "./images/image-11.jpeg";
 
 function App() {
   const images = [
-    { id: 1, image: img1 },
-    { id: 2, image: img2 },
-    { id: 3, image: img3 },
-    { id: 4, image: img4 },
-    { id: 5, image: img5 },
-    { id: 6, image: img6 },
-    { id: 7, image: img7 },
-    { id: 8, image: img8 },
-    { id: 9, image: img9 },
-    { id: 10, image: img10 },
-    { id: 11, image: img11 },
+    { id: 1, image: "https://i.ibb.co/8m4XDPK/image-1.webp" },
+    { id: 2, image: "https://i.ibb.co/wcVnv6r/image-2.webp" },
+    { id: 3, image: "https://i.ibb.co/LrKRXnJ/image-3.webp" },
+    { id: 4, image: "https://i.ibb.co/3dHsBqG/image-4.webp" },
+    { id: 5, image: "https://i.ibb.co/hYcgTPY/image-5.webp" },
+    { id: 6, image: "https://i.ibb.co/2Y0nZZP/image-6.webp" },
+    { id: 7, image: "https://i.ibb.co/W6h9fCj/image-7.webp" },
+    { id: 8, image: "https://i.ibb.co/fSqyK6N/image-8.webp" },
+    { id: 9, image: "https://i.ibb.co/kxZhRkw/image-9.webp" },
+    { id: 10, image: "https://i.ibb.co/DpqQcPX/image-10.jpg" },
+    { id: 11, image: "https://i.ibb.co/0DM4B4q/image-11.jpg" },
   ];
+
   return (
     <>
       <ImageGallery images={images} />
